262268e5b2c2ff325ed233cf4babf5d54b028e2b
Nico Rithner <nicorithner@gmail.com>
2023-03-28 17:01:09 -0600

    Add colors to constants

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const PRIMARY = {
+  chetwodeBlue: "hsla(234, 25%, 52%, 1)",
+  midnightExpress: "hsla(234, 53%, 16%, 1)",
+  frenchRose: "hsla(341, 92%, 62%, 1)",
+  carnationPink: "hsla(341, 100%, 83%, 1)",
+};
+
+export const NEUTRAL = {
+  white: "hsla(0, 0%, 100%, 1)",
+  gray: "hsla(233, 10%, 56%, 1)",
+};
+
+export const GRADIENTS = {
+  tomatoPersionRose: "linear-gradient(180deg, #FF6F48 0.04%, #F02AA6 100%)",
+  tomatoPersionRoseHSLA: "linear-gradient(180deg, hsla(13, 100%, 64%, 1), hsla(322, 87%, 55%, 1))",
+  bluePersianRose: "linear-gradient(180deg, #4851FF 0%, #F02AA6 99.92%)",
+  bluePersianRoseHSLA: "linear-gradient(180deg, hsla(237,100%, 64%, 1), hsla(322,87%, 55%, 1))",
+};
