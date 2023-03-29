7a737472f3d73654f15d252df604882feb1eed9a
Nico Rithner <nicorithner@gmail.com>
2023-03-28 19:10:04 -0600

    Change font import location. Add 500, 800 weights

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import GlobalStyles from './GlobalStyles';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import GlobalStyles from "./GlobalStyles";
+import "@fontsource/plus-jakarta-sans";
+import "@fontsource/plus-jakarta-sans/500.css";
+import "@fontsource/plus-jakarta-sans/800.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
