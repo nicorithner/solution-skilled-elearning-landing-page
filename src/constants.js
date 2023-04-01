0b99f1b074b9bcbf1c18354cb340250487cd0cdc
Nico Rithner <nicorithner@gmail.com>
2023-04-01 13:44:28 -0600

    Add white transparency gradient. Remove extra code

--- src/constants.js
+++ src/constants.js
@@ -11,8 +11,10 @@ export const NEUTRAL = {
 };
 
 export const GRADIENTS = {
-  tomatoPersionRose: "linear-gradient(180deg, #FF6F48 0.04%, #F02AA6 100%)",
-  tomatoPersionRoseHSLA: "linear-gradient(180deg, hsla(13, 100%, 64%, 1), hsla(322, 87%, 55%, 1))",
-  bluePersianRose: "linear-gradient(180deg, #4851FF 0%, #F02AA6 99.92%)",
-  bluePersianRoseHSLA: "linear-gradient(180deg, hsla(237,100%, 64%, 1), hsla(322,87%, 55%, 1))",
+  bluePersianRose:
+    "linear-gradient(180deg, hsla(237,100%, 64%, 1), hsla(322,87%, 55%, 1))",
+  tomatoPersionRose:
+    "linear-gradient(180deg, hsla(13, 100%, 64%, 1), hsla(322, 87%, 55%, 1))",
+  whiteTransparency:
+    "linear-gradient(0deg, hsla(0, 0%, 100%, 0.5), hsla(0, 0%, 100%, 0.5))",
 };
