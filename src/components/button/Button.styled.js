a97ce2799bfcdf9f9b78f1b58cb7484398578a55
Nico Rithner <nicorithner@gmail.com>
2023-04-01 13:43:26 -0600

    Style button options

--- src/components/button/Button.styled.js
+++ src/components/button/Button.styled.js
@@ -1,3 +1,40 @@
 import styled from "styled-components";
+import { GRADIENTS, NEUTRAL, PRIMARY } from "../../constants";
 
-export const ActionCallButton = styled.button``;
+const ButtonBase = styled.button`
+  background: ${NEUTRAL.gray};
+  border-radius: ${28 / 16}rem;
+  width: 167px;
+  height: 56px;
+  border: none;
+  padding: 14 32;
+  color: ${NEUTRAL.white};
+  font-family: "Plus Jakarta Sans";
+  font-style: normal;
+  font-size: ${18 / 16}rem;
+  font-weight: 700;
+  line-height: ${28 / 16}rem;
+  text-align: center;
+  cursor: pointer;
+`;
+export const ButtonSolid = styled(ButtonBase)`
+  background: ${PRIMARY.midnightExpress};
+
+  &:hover {
+    background: ${PRIMARY.chetwodeBlue};
+  }
+`;
+export const ButtonGrad1 = styled(ButtonBase)`
+  background: ${GRADIENTS.tomatoPersionRose};
+
+  &:hover {
+    background: ${GRADIENTS.whiteTransparency}, ${GRADIENTS.tomatoPersionRose};
+  }
+`;
+export const ButtonGrad2 = styled(ButtonBase)`
+  background: ${GRADIENTS.bluePersianRose};
+
+  &:hover {
+    background: ${GRADIENTS.whiteTransparency}, ${GRADIENTS.bluePersianRose};
+  }
+`;
