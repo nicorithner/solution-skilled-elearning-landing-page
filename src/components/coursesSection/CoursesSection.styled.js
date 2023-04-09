39f4cf8e0072238201ae2de71ea1cf18fa042d44
Nico Rithner <nicorithner@gmail.com>
2023-04-08 22:46:04 -0600

    Add style to courses section

--- src/components/coursesSection/CoursesSection.styled.js
+++ src/components/coursesSection/CoursesSection.styled.js
@@ -3,9 +3,20 @@ import styled from "styled-components";
 export const CardSection = styled.section`
   display: flex;
   flex-wrap: wrap;
-  gap: 30px;
+  justify-content: space-between;
   align-items: center;
   padding-left: 165px;
   padding-right: 165px;
-  padding-bottom: 140px;
+  padding-bottom: 60px;
+
+  @media only screen and (max-width: 768px) {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+
+  @media only screen and (max-width: 400px) {
+    padding: 0;
+    padding-bottom: 60px;
+    justify-content: center;
+  }
 `;
